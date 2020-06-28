5efc2c6634431c690fccf2c5de69735b732bedc4
Gabriel Agrela <sopustos@gmail.com>
2020-06-28 04:05:01 +0100

    - Some "taps" are now dynamic (percentage instead of pixel position); - Added email notification functionality (only sending email when CAPTCHA pops up, for now)

--- rok.bat
+++ rok.bat
@@ -2,5 +2,5 @@
 "C:\Users\Geremias\Desktop\platform-tools\adb.exe" kill-server
 "C:\Users\Geremias\Desktop\platform-tools\adb.exe" -s emulator-5554 shell input touchscreen swipe 50 820 50 820 200
 timeout /t 5
-cd C:\Users\Geremias
+cd C:\Users\Geremias\github\ROKBOT
 python rok.py
--- rok.py
+++ rok.py
@@ -4,11 +4,17 @@ from ppadb.client import Client
 from PIL import Image
 import numpy
 import time
-import threadings
+import time
+import threading
+import webcolors
+import yagmail
 
 inReset = False
 inAttack = False
 updateRunning = False
+
+xRes = 1920
+yRes = 1080
 
 adb = Client(host='127.0.0.1', port=5037)
 devices = adb.devices()
@@ -18,21 +24,25 @@ if len(devices) == 0:
 
 device = devices[0]
 
+
+def sendEmail(msg):
+	print ("sending email with ", msg, " as body.")
+	yag = yagmail.SMTP("ithrowthisaway1233321", '123456123456Aa')
+	yag.send("gabrielagrela99@gmail.com", "RISE OF KINGDOMS", msg)
 #troops died, need healing
 def reset():
 	global inReset
 	print("healing")
 	inReset = True
 	device.shell(f'input touchscreen swipe 50 950 50 950 200')  #tap city
-	time.sleep(1)
+	time.sleep(0.5)
 	device.shell(f'input touchscreen swipe 1747 620 1747 620 100 ')  #tap red cross
-	time.sleep(1)
+	time.sleep(0.5)
 	device.shell(f'input touchscreen swipe 1440 864 1440 864 100 ')  #tap heal button
-	time.sleep(1)
+	time.sleep(0.5)
 	device.shell(f'input touchscreen swipe 1755 630 1755 630 100 ')  #tap ask help healing
 	time.sleep(180) #wait for commander to return
 	device.shell(f'input touchscreen swipe 1747 620 1747 620 100 ') #tap recovered troops
-	time.sleep(1)
 	device.shell(f'input touchscreen swipe 50 950 50 950 200') #tap map
 
 	inReset = False
@@ -51,11 +61,9 @@ def attackFirstTime():
 	time.sleep(1)
 	device.shell(f'input touchscreen swipe 960 540 960 540 100 ') #tap barb
 	device.shell(f'input touchscreen swipe 1380 723 1380 723 100 ') #tap attack button
-	time.sleep(1)
 	device.shell(f'input touchscreen swipe 1630 215 1630 215 100 ') #tap new troops button
-	time.sleep(1)
 	device.shell(f'input touchscreen swipe 1651 745 1651 745 100 ') #tap button "4" (troops i want to select)
-	time.sleep(1)
+	time.sleep(0.2)
 	device.shell(f'input touchscreen swipe 1440 920 1440 920 100 ') #tap march button
 	inAttack=False
 	if(updateRunning == False):
@@ -67,13 +75,13 @@ def attack():
 	print("preparing attack")
 	inAttack=True
 	device.shell(f'input touchscreen swipe 50 820 50 820 200')#tap magnifying glass
+	time.sleep(0.2)
 	device.shell(f'input touchscreen swipe 400 750 400 750 10 ')#tap search button
 	time.sleep(1)
 	device.shell(f'input touchscreen swipe 960 540 960 540 100 ')#tap barb
+	time.sleep(0.1)
 	device.shell(f'input touchscreen swipe 1380 723 1380 723 100 ')#tap attack button
-	time.sleep(1)
 	device.shell(f'input touchscreen swipe 1830 320 1830 320 100 ') #tap army i want
-	time.sleep(1)
 	device.shell(f'input touchscreen swipe 1530 460 1530 460 100 ') #tap march button
 	inAttack=False
 
@@ -103,10 +111,15 @@ def update():
 		f.write(image)
 	image = Image.open('screen.png')
 	image = numpy.array(image, dtype=numpy.uint8) #get screenshot data in rgba
-	print (image[830][1467])
-	if ((image[830][1467][0] >= 220 and image[830][1467][0] <= 250) and (image[830][1467][1] >= 190 and image[830][1467][1] <= 225) and inReset == False): #if defeat notification
+	print (image[830][1467] , " - " , image[round(0.8*yRes)][round(0.8*xRes)] , " - " , image[round(0.1938*yRes)][round(0.8321*xRes)])
+	if (image[round(0.1938*yRes)][round(0.8321*xRes)][0] >= 220 and image[round(0.1938*yRes)][round(0.8321*xRes)][0] <= 255 and image[round(0.1938*yRes)][round(0.8321*xRes)][1] >= 225):
+		print("CAPTCHA")
+		sendEmail("CAPTCHA verification")
+	elif ((image[round(0.8*yRes)][round(0.8*xRes)][0] >= 0 and image[round(0.8*yRes)][round(0.8*xRes)][0] <= 50) and (image[round(0.8*yRes)][round(0.8*xRes)][1] >= 70 and image[round(0.8*yRes)][round(0.8*xRes)][1] <= 95) and (image[round(0.8*yRes)][round(0.8*xRes)][2] >= 100 and image[round(0.8*yRes)][round(0.8*xRes)][2] <= 125)): #if no action points
+		print ("no action points")
+	elif ((image[830][1467][0] >= 220 and image[830][1467][0] <= 250) and (image[830][1467][1] >= 190 and image[830][1467][1] <= 225) and inReset == False): #if defeat notification
 		reset()
-	if ((image[830][1467][1] >= 168 and image[830][1467][1] <= 175) and (image[830][1467][0] >= 35 and image[830][1467][0] <= 40) and inAttack == False): #if victory notification
+	elif ((image[830][1467][1] >= 160 and image[830][1467][1] <= 185) and (image[830][1467][0] >= 35 and image[830][1467][0] <= 80) and inAttack == False): #if victory notification
 		attack()
 
 attackFirstTime()
