a34c6982a01fea8854ceb7f6851d5bb4a494dda4
Gabriel Agrela <sopustos@gmail.com>
2020-06-30 18:27:27 +0100

    - Every pixel is now checked dynamically (position percentage relative to resolution, instead of fixed pixel)
    - Actions are now more precise and give less false positives (they use 
    two coordinates and its colors to determine the next action, instead of 
    one)
    
    - Started function (checkHospital()) to wait until troops are healed to 
    start attacking barbs again, instead of waiting a fixed amount of time.
    
    - Added iteration number and some comments

--- rok.py
+++ rok.py
@@ -18,26 +18,31 @@ nHelps = 0
 xRes = 1920
 yRes = 1080
 
+#start adb device
 adb = Client(host='127.0.0.1', port=5037)
 devices = adb.devices()
 if len(devices) == 0:
     print('no device attached')
     quit()
 
+#Prolly a good idea to have only 1 device while running this
 device = devices[0]
 
+#setting what the MAX R, G or B should be given a color value
 def setMax(color):
 	if(color >= 225):
 		return (255)
 	else:
 		return (color + 25)
 
+#setting what the MIN R, G or B should be given a color value
 def setMin(color):
 	if(color <= 25):
 		return (0)
 	else:
 		return (color - 25)
 
+#check if pixel color is within 25 units (positive or negative) in R G and B vectors
 def checkPixel (yPos, xPos, colorR, colorG, colorB, image):
 	colorRMax = setMax(colorR)
 	colorRMin = setMin(colorR)
@@ -48,7 +53,6 @@ def checkPixel (yPos, xPos, colorR, colorG, colorB, image):
 	colorBMax = setMax(colorB)
 	colorBMin = setMin(colorB)
 
-
 	if ((image[round(yPos*yRes)][round(xPos*xRes)][0] >= colorRMin and image[round(yPos*yRes)][round(xPos*xRes)][0] <= colorRMax) and (image[round(yPos*yRes)][round(xPos*xRes)][1] >= colorGMin and image[round(yPos*yRes)][round(xPos*xRes)][1] <= colorGMax) and (image[round(yPos*yRes)][round(xPos*xRes)][2] >= colorBMin and image[round(yPos*yRes)][round(xPos*xRes)][2] <= colorBMax)):
 		return True
 
@@ -58,10 +62,24 @@ def sendEmail(msg):
 	print ("sending email with ", msg, " as body.")
 	yag = yagmail.SMTP("ithrowthisaway1233321", '123456123456Aa')
 	yag.send("gabrielagrela99@gmail.com", "RISE OF KINGDOMS", msg)
-	sleeptTime = random.randint(0, 9)
+	sleeptTime = random.randint(0, 90)
 	time.sleep(sleeptTime)
-	device.shell(f'am force-stop com.lilithgame.roc.gp')  #tap city
+	#device.shell(f'am force-stop com.lilithgame.roc.gp')  #tap city
 
+# check if troops are healed
+def checkHopital():
+	image = device.screencap() #take screenshot
+	with open('screen2.png', 'wb') as f: #take screenshot
+		f.write(image)
+	image = Image.open('screen2.png')
+	image = numpy.array(image, dtype=numpy.uint8) #get screenshot data in rgba
+	time.sleep(5)
+	print("waiting for healing~: ", image[round(0.6163*yRes)][round(0.9302*xRes)])
+	if (checkPixel(0.6163,0.9302,230,230,235,image) == True):
+		time.sleep(5)
+		return checkHopital()
+	else:
+		return
 
 #troops died, need healing
 def reset():
@@ -77,7 +95,8 @@ def reset():
 	device.shell(f'input touchscreen swipe 1440 864 1440 864 100 ')  #tap heal button
 	time.sleep(0.5)
 	device.shell(f'input touchscreen swipe 1755 630 1755 630 100 ')  #tap ask help healing
-	time.sleep(180) #wait for commander to return
+	time.sleep(90)
+	#checkHopital()
 	device.shell(f'input touchscreen swipe 1747 620 1747 620 100 ') #tap recovered troops
 	device.shell(f'input touchscreen swipe 50 950 50 950 200') #tap map
 
@@ -107,6 +126,7 @@ def attackFirstTime():
 	if(updateRunning == False):
 		update()
 
+#Dynamic tap
 def tap (yPos, xPos):
 	cmd = 'input touchscreen swipe '+ str(round(xPos*xRes))+ ' '+ str(round(yPos*yRes))+' '+ str(round(xPos*xRes))+' '+ str(round(yPos*yRes))+' 100 '
 	device.shell(cmd)
@@ -157,19 +177,26 @@ def update():
 		f.write(image)
 	image = Image.open('screen.png')
 	image = numpy.array(image, dtype=numpy.uint8) #get screenshot data in rgba
+	print("\n ------------------------------------------------------------------------------------")
+	print("\n Iteration number: ")
 	print ("\n Helps: ", nHelps)
-	print ("\n color of the victory/defeat notification pixel: ", image[830][1467] , " \n color of a pixel of the 0AP pop-up: " , image[round(0.8*yRes)][round(0.8*xRes)] , " \n color of the position where the CAPTCHA notification pops-up " , image[round(0.1938*yRes)][round(0.8321*xRes)] , " \n color of the position where the help notification pops-up " , image[round(0.6343*yRes)][round(0.9804*xRes)] , "\n --------------------------------------------------------------------------")
-	if (checkPixel(0.1938,0.8321,230,230,235,image) == True and inEmail == False):
+	print ("\n New victory cords: ", image[round(0.7597*yRes)][round(0.7979*xRes)], " and ", image[round(0.77*yRes)][round(0.7631*xRes)])
+	print ("\n New deafeat cords: ", image[round(0.7726*yRes)][round(0.7616*xRes)], " and ", image[round(0.7713*yRes)][round(0.8001*xRes)])
+	print ("\n New CAPTCHA cords: ", image[round(0.1938*yRes)][round(0.8147*xRes)], " and ", image[round(0.1938*yRes)][round(0.8321*xRes)])
+	print ("\n New AP cords: ", image[round(0.5633*yRes)][round(0.2296*xRes)], " and ", image[round(0.1938*yRes)][round(0.8321*xRes)])
+	#print ("\n color of the victory/defeat notification pixel: ", image[830][1467] , " \n color of a pixel of the 0AP pop-up: " , image[round(0.8*yRes)][round(0.8*xRes)] , " \n color of the position where the CAPTCHA notification pops-up " , image[round(0.1938*yRes)][round(0.8321*xRes)] , " \n color of the position where the help notification pops-up " , image[round(0.6343*yRes)][round(0.9804*xRes)] , "\n --------------------------------------------------------------------------")
+	if (checkPixel(0.1938,0.8147,251,252,251,image) == True and checkPixel(0.1938,0.8321,230,230,235,image) == True and inEmail == False): #if theres a CAPTCHA
 		print(" I think there's a CAPTCHA")
 		sendEmail("CAPTCHA verification")
-	elif ((image[round(0.8*yRes)][round(0.8*xRes)][0] >= 0 and image[round(0.8*yRes)][round(0.8*xRes)][0] <= 50) and (image[round(0.8*yRes)][round(0.8*xRes)][1] >= 70 and image[round(0.8*yRes)][round(0.8*xRes)][1] <= 95) and (image[round(0.8*yRes)][round(0.8*xRes)][2] >= 100 and image[round(0.8*yRes)][round(0.8*xRes)][2] <= 125)): #if no action points
+	elif (checkPixel(0.5633,0.2296,255,166,58,image) == True and checkPixel(0.1938,0.8321,0,73,107,image) == True  and inEmail == False ): #if no action points
 		print (" I think you have no action points")
-	elif ((image[830][1467][0] >= 220 and image[830][1467][0] <= 250) and (image[830][1467][1] >= 190 and image[830][1467][1] <= 225) and inReset == False): #if defeat notification
+		sendEmail("NO ACTION POINTS")
+	elif (checkPixel(0.7726,0.7616,254,143,144,image) == True and checkPixel(0.7713,0.8001,251,146,146,image) == True and inReset == False): #if defeat notification
 		reset()
-	elif ((image[830][1467][1] >= 160 and image[830][1467][1] <= 185) and (image[830][1467][0] >= 35 and image[830][1467][0] <= 80) and inAttack == False): #if victory notification
+	elif (checkPixel(0.7597,0.7979,27,169,49,image) == True and checkPixel(0.77,0.7631,8,162,33,image) == True and inAttack == False): #if victory notification
 		attack()
-	elif (checkPixel(0.6343,0.9804,230,0,0,image) == True):
-		inEmail= False
+	elif (checkPixel(0.6343,0.9804,230,0,0,image) == True): #if theres a alliance help request
+		inEmail = False
 		tap(0.67,0.96)
 		nHelps = nHelps + 1
 
