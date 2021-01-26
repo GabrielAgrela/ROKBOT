c33b5e0ecfbcad2456016fbc20211c6d25fced1f
Darren Lai <kinwah.lai@gmail.com>
2021-01-26 15:07:34 +0800

    fix for mac and nox player use

---
+++ start-nox.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# winsound missing in mac, replace with something else
+
+# tkinter also need special handling in mac 
+# https://medium.com/@xogk39/install-tkinter-on-mac-pyenv-f112bd3f4663
+
+# install tesseract via brew
+
+# install android-platform-tools with brew or set path to Android Studio sdk/platform-tools
+
+
+
+# connect to Nox app player
+adb connect 127.0.0.1:62001
+python rok.py 
